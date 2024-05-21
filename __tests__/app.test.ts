d41543d154eb79b1edf8bb4c8efa2e474dbf0d72
Daniel Williams <autoboots@gmail.com>
2024-05-21 16:01:24 +0100

    adds local address to test file to use in requests

--- __tests__/app.test.ts
+++ __tests__/app.test.ts
@@ -7,59 +7,60 @@ import connect from "../lib/index";
 import * as mongoDB from "mongodb";
 const request = require("supertest");
 
+const local: string = "http://localhost:3000";
+
 describe("seed", () => {
-  let client: mongoDB.MongoClient;
-  let db: mongoDB.Db;
+    let client: mongoDB.MongoClient;
+    let db: mongoDB.Db;
 
-  beforeAll(async () => {
-    client = await connect();
-    db = client.db("test");
-  });
-
-  beforeEach(async () => {
-    await runSeed(usersData, flashcardsData, forumsData, articlesData);
-  });
-
-  afterAll(async () => {
-    await client.close();
-  });
-
-  describe("seed()", () => {
-    test("seed should insert data into the users collection", async () => {
-      const users = db.collection("users");
-      const count = await users.countDocuments();
-
-      expect(count).toBe(5);
+    beforeAll(async () => {
+        client = await connect();
+        db = client.db("test");
     });
-    test("seed should insert data into the articles collection", async () => {
-      const articles = db.collection("articles");
-      const count = await articles.countDocuments();
 
-      expect(count).toBe(5);
+    beforeEach(async () => {
+        await runSeed(usersData, flashcardsData, forumsData, articlesData);
     });
-    test("seed should insert data into the forums collection", async () => {
-      const forums = db.collection("forums");
-      const count = await forums.countDocuments();
 
-      expect(count).toBe(5);
+    afterAll(async () => {
+        await client.close();
     });
-    test("seed should insert data into the flashcards collection", async () => {
-      const flashcards = db.collection("flashcards");
-      const count = await flashcards.countDocuments();
 
-      expect(count).toBe(5);
-    });
-  });
+    describe("seed()", () => {
+        test("seed should insert data into the users collection", async () => {
+            const users = db.collection("users");
+            const count = await users.countDocuments();
 
-  describe("/api/users", () => {
-    test("Should return an array", async () => {
-      const response = await request("app here")
-      .get("/api/users")
-      
-      console.log(response);
-      
-      expect(response.status).toBe(200)
-      expect(typeof response.body).toEqual('object');
+            expect(count).toBe(5);
+        });
+        test("seed should insert data into the articles collection", async () => {
+            const articles = db.collection("articles");
+            const count = await articles.countDocuments();
+
+            expect(count).toBe(5);
+        });
+        test("seed should insert data into the forums collection", async () => {
+            const forums = db.collection("forums");
+            const count = await forums.countDocuments();
+
+            expect(count).toBe(5);
+        });
+        test("seed should insert data into the flashcards collection", async () => {
+            const flashcards = db.collection("flashcards");
+            const count = await flashcards.countDocuments();
+
+            expect(count).toBe(5);
+        });
     });
-  });
+
+    describe("/api/users", () => {
+        test("Should return an array", async () => {
+            const response = await request(local).get("/api/users");
+
+            // console.log(response);
+
+            expect(response.status).toBe(200);
+            expect(typeof response.body).toEqual("object");
+        });
+    });
 });
