2617e654f4eb2c31241c4e6eeac52242920cb6b5
Daniel Williams <autoboots@gmail.com>
2024-05-22 16:28:52 +0100

    adds test for undefined routes, and adds [...slug] route to send 404 message for any nonexistent endpoint

--- __tests__/app.test.ts
+++ __tests__/app.test.ts
@@ -10,100 +10,113 @@ import { NextRequest, NextResponse } from "next/server";
 
 import { GET as getAllUsers } from "../app/api/users/route";
 import { GET as getUserById } from "../app/api/users/[id]/route";
+import { GET as getCatchAll } from "../app/api/[...slug]/route";
 
 let client: mongoDB.MongoClient;
 let db: mongoDB.Db;
 
 beforeAll(async () => {
-  client = await connect();
-  db = client.db("test");
+    client = await connect();
+    db = client.db("test");
 });
 
 beforeEach(async () => {
-  await runSeed(usersData, flashcardsData, forumsData, articlesData);
+    await runSeed(usersData, flashcardsData, forumsData, articlesData);
 });
 
 afterAll(async () => {
-  await client.close();
+    await client.close();
 });
 
 describe("seed()", () => {
-  test("seed should insert data into the users collection", async () => {
-    const users = db.collection("users");
-    const count = await users.countDocuments();
+    test("seed should insert data into the users collection", async () => {
+        const users = db.collection("users");
+        const count = await users.countDocuments();
 
-    expect(count).toBe(5);
-  });
-  test("seed should insert data into the articles collection", async () => {
-    const articles = db.collection("articles");
-    const count = await articles.countDocuments();
+        expect(count).toBe(5);
+    });
+    test("seed should insert data into the articles collection", async () => {
+        const articles = db.collection("articles");
+        const count = await articles.countDocuments();
 
-    expect(count).toBe(5);
-  });
-  test("seed should insert data into the forums collection", async () => {
-    const forums = db.collection("forums");
-    const count = await forums.countDocuments();
+        expect(count).toBe(5);
+    });
+    test("seed should insert data into the forums collection", async () => {
+        const forums = db.collection("forums");
+        const count = await forums.countDocuments();
 
-    expect(count).toBe(5);
-  });
-  test("seed should insert data into the flashcards collection", async () => {
-    const flashcards = db.collection("flashcards");
-    const count = await flashcards.countDocuments();
+        expect(count).toBe(5);
+    });
+    test("seed should insert data into the flashcards collection", async () => {
+        const flashcards = db.collection("flashcards");
+        const count = await flashcards.countDocuments();
 
-    expect(count).toBe(5);
-  });
+        expect(count).toBe(5);
+    });
+});
+
+describe("/api/any-undefined-route", () => {
+    test("Non existent endpoints should return 404", async () => {
+        const req = {} as NextRequest;
+        const res = (await getCatchAll(req)) as NextResponse;
+
+        const data = await res.json();
+
+        expect(res.status).toBe(404);
+        expect(data.error).toBe("404 Error: Resource doesn't exist");
+    });
 });
 
 describe("/api/users", () => {
-  test("Should return an array", async () => {
-    const req = {} as NextRequest;
-    const res = (await getAllUsers(req)) as NextResponse;
+    test("Should return an array", async () => {
+        const req = {} as NextRequest;
+        const res = (await getAllUsers(req)) as NextResponse;
 
-    const data = await res.json();
+        const data = await res.json();
 
-    expect(res.status).toBe(200);
-    expect(Array.isArray(data.users)).toBe(true);
+        expect(res.status).toBe(200);
+        expect(Array.isArray(data.users)).toBe(true);
 
-    data.users.forEach((user: any) => {
-      expect(user).toHaveProperty("_id");
-      expect(user).toHaveProperty("full_name");
-      expect(user).toHaveProperty("email");
-      expect(user).toHaveProperty("password");
-      expect(user).toHaveProperty("bookmarks");
-      expect(user).toHaveProperty("progress");
+        data.users.forEach((user: any) => {
+            expect(user).toHaveProperty("_id");
+            expect(user).toHaveProperty("full_name");
+            expect(user).toHaveProperty("email");
+            expect(user).toHaveProperty("password");
+            expect(user).toHaveProperty("bookmarks");
+            expect(user).toHaveProperty("progress");
+        });
     });
-  });
 });
 
 describe("/api/users/:_id", () => {
-  test("Should return data associated with specified id", async () => {
-    const req = {} as NextRequest;
-    const params = { params: { id: "664db5ae509cc0afb30cc382" } };
-    const res = (await getUserById(req, params)) as NextResponse;
+    test("Should return data associated with specified id", async () => {
+        const req = {} as NextRequest;
+        const params = { params: { id: "664db5ae509cc0afb30cc382" } };
+        const res = (await getUserById(req, params)) as NextResponse;
 
-    const data = await res.json();
+        const data = await res.json();
 
-    expect(res.status).toBe(200);
-    expect(data.user.full_name).toBe("Alex Johnson");
-  });
-  test("Should return a 404 error for non-existent id", async () => {
-    const req = {} as NextRequest;
-    const params = { params: { id: "664d9e9f509cc0afb30cc369" } };
-    const res = (await getUserById(req, params)) as NextResponse;
+        expect(res.status).toBe(200);
+        expect(data.user.full_name).toBe("Alex Johnson");
+    });
+    test("Should return a 404 error for non-existent id", async () => {
+        const req = {} as NextRequest;
+        const params = { params: { id: "664d9e9f509cc0afb30cc369" } };
+        const res = (await getUserById(req, params)) as NextResponse;
 
-    const user = await res.json();
+        const user = await res.json();
 
-    expect(res.status).toBe(404);
-    expect(user.error).toBe("404 Error: Resource doesn't exist");
-  });
-  test("Should return a 400 error for an invalid id Type", async () => {
-    const req = {} as NextRequest;
-    const params = { params: { id: "non-valid-id-string" } };
-    const res = (await getUserById(req, params)) as NextResponse;
+        expect(res.status).toBe(404);
+        expect(user.error).toBe("404 Error: Resource doesn't exist");
+    });
+    test("Should return a 400 error for an invalid id Type", async () => {
+        const req = {} as NextRequest;
+        const params = { params: { id: "non-valid-id-string" } };
+        const res = (await getUserById(req, params)) as NextResponse;
 
-    const user = await res.json();
+        const user = await res.json();
 
-    expect(res.status).toBe(400);
-    expect(user.error).toBe("400 Error: Invalid ID Syntax");
-  });
+        expect(res.status).toBe(400);
+        expect(user.error).toBe("400 Error: Invalid ID Syntax");
+    });
 });
