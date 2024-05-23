274e41a6af5d7d6816ba7c209f520a324aed83c5
Ellie <admin@Ellies-Macbook.local>
2024-05-23 11:36:20 +0100

    add 201, 201, 400, 400 tests for post user

--- __tests__/app.test.ts
+++ __tests__/app.test.ts
@@ -9,6 +9,7 @@ import * as mongoDB from "mongodb";
 import { NextRequest, NextResponse } from "next/server";
 
 import { GET as getAllUsers } from "../app/api/users/route";
+import { POST as postUser } from "../app/api/users/route";
 import { GET as getUserById } from "../app/api/users/[id]/route";
 
 let client: mongoDB.MongoClient;
@@ -55,22 +56,123 @@ describe("seed()", () => {
 });
 
 describe("/api/users", () => {
-  test("Should return an array", async () => {
-    const req = {} as NextRequest;
-    const res = (await getAllUsers(req)) as NextResponse;
+  describe("GET", () => {
+    test("Should return an array of all users", async () => {
+      const req = {} as NextRequest;
+      const res = (await getAllUsers(req)) as NextResponse;
 
-    const data = await res.json();
+      const data = await res.json();
 
-    expect(res.status).toBe(200);
-    expect(Array.isArray(data.users)).toBe(true);
+      expect(res.status).toBe(200);
+      expect(Array.isArray(data.users)).toBe(true);
+      expect(data.users.length).toBe(5);
 
-    data.users.forEach((user: any) => {
-      expect(user).toHaveProperty("_id");
-      expect(user).toHaveProperty("full_name");
-      expect(user).toHaveProperty("email");
-      expect(user).toHaveProperty("password");
-      expect(user).toHaveProperty("bookmarks");
-      expect(user).toHaveProperty("progress");
+      data.users.forEach((user: any) => {
+        expect(user).toHaveProperty("_id");
+        expect(user).toHaveProperty("full_name");
+        expect(user).toHaveProperty("email");
+        expect(user).toHaveProperty("password");
+        expect(user).toHaveProperty("bookmarks");
+        expect(user).toHaveProperty("progress");
+      });
+    });
+  });
+
+  describe("POST", () => {
+    test("POST 201: Should return a 201 status code with the comment body", async () => {
+      const mockJson = jest.fn().mockResolvedValue({
+        body: {
+          username: "sofiac",
+          full_name: "Sofia Carlos",
+          email: "sofia.c1996@example.com",
+          password: "puppies96",
+        },
+      });
+
+      const req = {
+        json: mockJson,
+      } as unknown as NextRequest;
+
+      const res = (await postUser(req)) as NextResponse;
+
+      const data = await res.json();
+
+      expect(res.status).toBe(201);
+      expect(data.acknowledged).toBe(true);
+      expect(data).toHaveProperty("_id");
+      expect(data).toHaveProperty("username");
+      expect(data).toHaveProperty("full_name");
+      expect(data).toHaveProperty("email");
+      expect(data).toHaveProperty("password");
+      expect(data).toHaveProperty("bookmarks");
+      expect(data).toHaveProperty("progress");
+      expect(data).not.toHaveProperty("age");
+    });
+    test("POST 201: Should ignore extra inputs", async () => {
+      const mockJson = jest.fn().mockResolvedValue({
+        body: {
+          full_name: "Sofia Carlos",
+          username: "sofiac",
+          email: "sodia.c1996@example.com",
+          password: "puppies96",
+          age: 42,
+        },
+      });
+
+      const req = {
+        json: mockJson,
+      } as unknown as NextRequest;
+
+      const res = (await postUser(req)) as NextResponse;
+
+      const data = await res.json();
+
+      expect(res.status).toBe(201);
+      expect(data.acknowledged).toBe(true);
+      expect(data).toHaveProperty("_id");
+      expect(data).toHaveProperty("full_name");
+      expect(data).toHaveProperty("email");
+      expect(data).toHaveProperty("password");
+      expect(data).toHaveProperty("bookmarks");
+      expect(data).toHaveProperty("progress");
+      expect(data).not.toHaveProperty("age");
+    });
+    test("POST 400: Should return an error when the request body is malformed/ has missing fields", async () => {
+      const mockJson = jest.fn().mockResolvedValue({
+        body: {},
+      });
+
+      const req = {
+        json: mockJson,
+      } as unknown as NextRequest;
+
+      const res = (await postUser(req)) as NextResponse;
+
+      const data = await res.json();
+
+      expect(res.status).toBe(400);
+      expect(data.msg).toBe("400 Error: Missing/Malformed fields");
+    });
+    test("POST 400: Should return an error when the username already exists in the database", async () => {
+      const mockJson = jest.fn().mockResolvedValue({
+        body: {
+          username: "parentpro",
+          full_name: "Alex Johnson",
+          email: "alex.johnson@example.com",
+          password: "P@ssw0rd123",
+        },
+      });
+
+      const req = {
+        json: mockJson,
+      } as unknown as NextRequest;
+
+      const res = (await postUser(req)) as NextResponse;
+
+      const data = await res.json();
+
+      expect(res.status).toBe(400)
+      expect(data.msg).toBe("400 Error: Username Already Exists!");
     });
   });
 });
