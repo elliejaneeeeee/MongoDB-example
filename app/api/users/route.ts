ea4dc78b932152a914e6bfd09f1c1865fcaf0a94
Ellie <admin@Ellies-Macbook.local>
2024-05-23 11:36:58 +0100

    add controller for post users

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -1,9 +1,35 @@
 import { NextRequest, NextResponse } from "next/server";
-import { fetchAllUsers } from "../../../models/users.models";
+import { fetchAllUsers, insertUser } from "../../../models/users.models";
+import { error, userResponse } from "@/types";
 
 async function GET(req: NextRequest) {
-  const users = await fetchAllUsers()
-  return NextResponse.json({ users } , {status: 200 })
+  const users = await fetchAllUsers();
+  return NextResponse.json({ users }, { status: 200 });
 }
 
-export { GET };
+async function POST(req: Request) {
+  const parsedRequest = await req.json();
+  const { username, full_name, email, password } = parsedRequest.body;
+
+  if (!username || !full_name || !email || !password) {
+    return NextResponse.json(
+      { msg: "400 Error: Missing/Malformed fields" },
+      { status: 400 }
+    );
+  }
+
+  const res: any = await insertUser(
+    username,
+    full_name,
+    email,
+    password
+  )!;
+
+  if (res.error) {
+    return NextResponse.json({msg: res.error}, { status: res.status });
+  }
+
+  return NextResponse.json(res, { status: 201 });
+}
+
+export { GET, POST };
