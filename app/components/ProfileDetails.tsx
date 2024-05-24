bc8f83346d3d4814faa81eb76cc76d640eaca197
Daniel Williams <autoboots@gmail.com>
2024-05-24 20:08:19 +0100

    removes unused redirect from ProfileDetails component

--- app/components/ProfileDetails.tsx
+++ app/components/ProfileDetails.tsx
@@ -2,7 +2,6 @@
 import React from "react";
 import { signOut } from "next-auth/react";
 import { useSession } from "next-auth/react";
-import { redirect } from "next/navigation";
 
 const ProfileDetails = () => {
     const { data: session } = useSession();
