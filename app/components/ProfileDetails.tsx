bc535b09c9dead9514525493870ccbfc47fadfda
Daniel Williams <autoboots@gmail.com>
2024-05-24 20:05:57 +0100

    refactors session handling for login page out of component to match login and register

--- app/components/ProfileDetails.tsx
+++ app/components/ProfileDetails.tsx
@@ -7,10 +7,6 @@ import { redirect } from "next/navigation";
 const ProfileDetails = () => {
     const { data: session } = useSession();
 
-    if (session == null) {
-        return redirect("/login");
-    }
-
     return (
         <div className="grid place-items-center h-screen">
             <div className="shadow-lg p-8 bg-zince-300/10 flex flex-col gap-2 my-6">
