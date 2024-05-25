ac8c228a57334e8e5c192f82a88888c59d2688c3
elliejaneeeeee <145994573+elliejaneeeeee@users.noreply.github.com>
2024-05-25 23:23:58 +0100

    add providers

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,14 +1,7 @@
-// app/providers.tsx
 "use client";
-
-import { ChakraProvider } from "@chakra-ui/react";
 import { SessionProvider } from "next-auth/react";
 import React from "react";
 
-export function Providers({ children }: { children: React.ReactNode }) {
-  return <ChakraProvider>{children}</ChakraProvider>;
-}
-
-export const AuthProvider = ({ children }: { children: React.ReactNode} ) => {
-  return <SessionProvider>{children}</SessionProvider>
-}
+export const AuthProvider = ({ children }) => {
+    return <SessionProvider>{children}</SessionProvider>;
+};
