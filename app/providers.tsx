d97c54082e0db03624563bf908f6763f160cf2dc
elliejaneeeeee <145994573+elliejaneeeeee@users.noreply.github.com>
2024-05-25 23:19:38 +0100

    resolve merge conficts

--- app/providers.tsx
+++ app/providers.tsx
@@ -2,7 +2,13 @@
 "use client";
 
 import { ChakraProvider } from "@chakra-ui/react";
+import { SessionProvider } from "next-auth/react";
+import React from "react";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return <ChakraProvider>{children}</ChakraProvider>;
+}
+
+export const AuthProvider = ({ children }) => {
+  return <SessionProvider>{children}</SessionProvider>
 }
