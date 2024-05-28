a050090da342ca9c9f947bdbf81b673c5f7cfcd6
elliejaneeeeee <145994573+elliejaneeeeee@users.noreply.github.com>
2024-05-28 15:19:40 +0100

    Update users.ts

--- lib/testdata/users.ts
+++ lib/testdata/users.ts
@@ -18,10 +18,8 @@ export const usersData: users[] = [
         username: "parentpro",
         full_name: "Alex Johnson",
         email: "alex.johnson@example.com",
-        password:
-            "$2a$10$6bqGx0BuW2KMWOXp7SLxy.Zz/uXxdxmZE0rOkSiyw8fDZ4q3/azly",
+        password: "$2a$10$6bqGx0BuW2KMWOXp7SLxy.Zz/uXxdxmZE0rOkSiyw8fDZ4q3/azly",
         bookmarks: [],
-
         progress: [
             { lesson1: true },
             { lesson2: false },
@@ -34,8 +32,7 @@ export const usersData: users[] = [
         username: "sleepguru",
         full_name: "Maria Rodriguez",
         email: "maria.rodriguez@example.com",
-        password:
-            "$2a$10$nVSY1oIMt4lfvWCRR9CzXesCIBecqFdA6dmjCetgzTZGF1tvAE8u6",
+        password: "$2a$10$nVSY1oIMt4lfvWCRR9CzXesCIBecqFdA6dmjCetgzTZGF1tvAE8u6",
         bookmarks: [],
 
         progress: [
@@ -50,9 +47,8 @@ export const usersData: users[] = [
         username: "dadoftwins",
         full_name: "David Lee",
         email: "david.lee@example.com",
-        password:
-            "$2a$10$1clpr2YlvFE4NP4jziNL9eR22MdzkD.XFUfhdI5DoCwDju9fZv.UW",
-        bookmarks: [ {_id: '664d9e9f509cc0afb30cc369', type: 'articles' }],
+        password: "$2a$10$1clpr2YlvFE4NP4jziNL9eR22MdzkD.XFUfhdI5DoCwDju9fZv.UW",
+        bookmarks: ['664d9e9f509cc0afb30cc369'],
         progress: [
             { lesson1: false },
             { lesson2: true },
@@ -65,9 +61,8 @@ export const usersData: users[] = [
         username: "nutritionistmom",
         full_name: "Sophie Brown",
         email: "sophie.brown@example.com",
-        password:
-            "$2a$10$H9BI.esDl6rqWhNQVeppZe./VJVTzFLqJWD88e7Zi5KiqawmMbdrC",
-        bookmarks: [{_id: '664d9e9f509cc0afb30cc369', type: 'articles' }],
+        password: "$2a$10$H9BI.esDl6rqWhNQVeppZe./VJVTzFLqJWD88e7Zi5KiqawmMbdrC",
+        bookmarks: ['664d9e9f509cc0afb30cc369'],
         progress: [
             { lesson1: true },
             { lesson2: true },
@@ -80,8 +75,7 @@ export const usersData: users[] = [
         username: "newdad123",
         full_name: "Liam Smith",
         email: "liam.smith@example.com",
-        password:
-            "$2a$10$cjdw4rQpZi45//eXiRkdJuwZjqGjQbsQxPrJES2jylH9HPFSfbTzy",
+        password: "$2a$10$cjdw4rQpZi45//eXiRkdJuwZjqGjQbsQxPrJES2jylH9HPFSfbTzy",
         bookmarks: [],
         progress: [
             { lesson1: true },
