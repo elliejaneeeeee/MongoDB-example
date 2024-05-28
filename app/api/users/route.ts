63cabad40bf97afddfa89ac214385cfd615fbdc2
elliejaneeeeee <145994573+elliejaneeeeee@users.noreply.github.com>
2024-05-28 15:18:58 +0100

    Update route.ts

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -1,25 +1,9 @@
 import { NextRequest, NextResponse } from "next/server";
-import { fetchAll, insertUser } from "../../../models/utils";
+import { fetchAll } from "../../../models/utils";
 
 async function GET(req: NextRequest) {
   const users = await fetchAll({coll: 'users'})
   return NextResponse.json({ users } , {status: 200 })
 }
 
-async function POST(req: Request) {
-  const parsedRequest = await req.json();
-  try {
-    const { username, full_name, email, password } = parsedRequest.body;
-
-    const res: any = await insertUser(username, full_name, email, password)!;
-
-    return NextResponse.json(res, { status: 201 });
-  } catch (error: any) {
-    return NextResponse.json(
-      { error: "400 Error: Bad Request!" },
-      { status: 400 }
-    );
-  }
-}
-
-export { GET, POST };
+export { GET };
