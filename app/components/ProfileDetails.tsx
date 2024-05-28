5ff1a5f8c28354a80735956bc2bc7080c59ffa6f
Daniel Williams <autoboots@gmail.com>
2024-05-28 21:01:20 +0100

    adds NavBar to profile page, adds loading message while session is loading

--- app/components/ProfileDetails.tsx
+++ app/components/ProfileDetails.tsx
@@ -4,25 +4,35 @@ import { signOut } from "next-auth/react";
 import { useSession } from "next-auth/react";
 
 const ProfileDetails = () => {
-    const { data: session } = useSession();
+    const { data: session, status } = useSession();
 
     return (
         <div className="grid place-items-center h-screen">
             <div className="shadow-lg p-8 bg-zince-300/10 flex flex-col gap-2 my-6">
-                <div>
-                    Name:
-                    <span className="font-bold">{session?.user?.name}</span>
-                </div>
-                <div>
-                    Email:
-                    <span className="font-bold">{session?.user?.email}</span>
-                </div>
-                <button
-                    onClick={() => signOut({ callbackUrl: "/login" })}
-                    className="bg-red-500 text-white font-bold px-6 py-2 mt-3"
-                >
-                    Log Out
-                </button>
+                {status === "loading" ? (
+                    <div>Loading profile...</div>
+                ) : (
+                    <>
+                        <div>
+                            Name:
+                            <span className="font-bold">
+                                {session?.user?.name}
+                            </span>
+                        </div>
+                        <div>
+                            Email:
+                            <span className="font-bold">
+                                {session?.user?.email}
+                            </span>
+                        </div>
+                        <button
+                            onClick={() => signOut({ callbackUrl: "/login" })}
+                            className="bg-red-500 text-white font-bold px-6 py-2 mt-3"
+                        >
+                            Log Out
+                        </button>
+                    </>
+                )}
             </div>
         </div>
     );
--- types.ts
+++ types.ts
@@ -1,89 +1,93 @@
 import { ObjectId } from "mongodb";
 
 export class CustomError extends Error {
-  status: errorMsg
-  
-  constructor(msg: string, status: number) {
-    super(msg)
-    this.status = { code: status, msg}
-  }
+    status: errorMsg;
+
+    constructor(msg: string, status: number) {
+        super(msg);
+        this.status = { code: status, msg };
+    }
 }
 export interface errorMsg {
-  code: number,
-  msg: string
+    code: number;
+    msg: string;
 }
 
 export interface userResponse {
-  acknowledged: boolean;
-  insertedId: ObjectId;
-  _id: ObjectId;
-  username: string;
-  full_name: string;
-  email: string;
-  password: string;
-  bookmarks: number;
-  progress: Lesson[] | [];
+    acknowledged: boolean;
+    insertedId: ObjectId;
+    _id: ObjectId;
+    username: string;
+    full_name: string;
+    email: string;
+    password: string;
+    bookmarks: number;
+    progress: Lesson[] | [];
 }
 
 export interface comments {
-  _id: ObjectId;
-  author: string;
-  body: string;
-  votes: number;
-  date: Date;
+    _id: ObjectId;
+    author: string;
+    body: string;
+    votes: number;
+    date: Date;
 }
 
 export interface forums {
-  _id: ObjectId;
-  title: string;
-  body: string;
-  author: string;
-  votes: number;
-  date: Date;
-  comments: comments[];
+    _id: ObjectId;
+    title: string;
+    body: string;
+    author: string;
+    votes: number;
+    date: Date;
+    comments: comments[];
 }
 
 interface Lesson {
-  [key: `lesson${number}`]: boolean;
+    [key: `lesson${number}`]: boolean;
 }
 
 export interface users {
-  _id: ObjectId;
-  username: string;
-  full_name: string;
-  email: string;
-  password: string;
-  bookmarks: string[];
-  progress: Lesson[];
+    _id: ObjectId;
+    username: string;
+    full_name: string;
+    email: string;
+    password: string;
+    bookmarks: string[];
+    progress: Lesson[];
 }
 
 export interface flashcards {
-  _id: ObjectId;
-  unit: number;
-  section: string;
-  title: string;
-  body: string[];
-  img_url: string;
+    _id: ObjectId;
+    unit: number;
+    section: string;
+    title: string;
+    body: string[];
+    img_url: string;
+}
+
+export interface flashcardsResponse {
+    flashcards: flashcards[];
 }
 
 export interface articles {
-  _id: ObjectId;
-  title: string;
-  link: string;
-  img_url: string;
-  body: string;
-  source: string;
-  votes: number;
+    _id: ObjectId;
+    title: string;
+    link: string;
+    img_url: string;
+    body: string;
+    source: string;
+    votes: number;
 }
 
 export interface updateFields {
-  username?: string,
-  password?: string,
-  full_name?: string,
-  email?: string,
+    username?: string;
+    password?: string;
+    full_name?: string;
+    email?: string;
 }
 
 export interface Saves {
-  type: string
-  _id: string
+    type: string;
+    _id: string;
 }
