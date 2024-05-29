d1b7ffdacbeceb35b10787d2a4cdd9f436942016
Sophie <sophie.v.p@hotmail.co.uk>
2024-05-29 15:23:07 +0100

    changes types to "type" instead of "interface"

--- types.ts
+++ types.ts
@@ -1,107 +1,106 @@
 import { ObjectId } from "mongodb";
 import NextAuth, { DefaultSession, DefaultUser } from "next-auth";
 
-
 declare module "next-auth" {
-    interface Session {
-        user: {
-            id: string;
-        } & DefaultSession["user"];
-    }
+  interface Session {
+    user: {
+      id: string;
+    } & DefaultSession["user"];
+  }
 
-    interface User extends DefaultUser {
-        id: string;
-    }
+  interface User extends DefaultUser {
+    id: string;
+  }
 }
 export class CustomError extends Error {
-    status: errorMsg;
+  status: errorMsg;
 
-    constructor(msg: string, status: number) {
-        super(msg);
-        this.status = { code: status, msg };
-    }
+  constructor(msg: string, status: number) {
+    super(msg);
+    this.status = { code: status, msg };
+  }
 }
-export interface errorMsg {
-    code: number;
-    msg: string;
-}
+export type errorMsg = {
+  code: number;
+  msg: string;
+};
 
-export interface userResponse {
-    acknowledged: boolean;
-    insertedId: ObjectId;
-    _id: ObjectId;
-    username: string;
-    full_name: string;
-    email: string;
-    password: string;
-    bookmarks: number;
-    progress: Lesson[] | [];
-}
+export type userResponse = {
+  acknowledged: boolean;
+  insertedId: ObjectId;
+  _id: ObjectId;
+  username: string;
+  full_name: string;
+  email: string;
+  password: string;
+  bookmarks: number;
+  progress: Lesson[] | [];
+};
 
-export interface comments {
-    _id: ObjectId;
-    author: string;
-    body: string;
-    votes: number;
-    date: Date;
-}
+export type comments = {
+  _id: ObjectId;
+  author: string;
+  body: string;
+  votes: number;
+  date: Date;
+};
 
-export interface forums {
-    _id: ObjectId;
-    title: string;
-    body: string;
-    author: string;
-    votes: number;
-    date: Date;
-    comments: comments[];
-}
+export type forums = {
+  _id: ObjectId;
+  title: string;
+  body: string;
+  author: string;
+  votes: number;
+  date: Date;
+  comments: comments[];
+};
 
 interface Lesson {
-    [key: `lesson${number}`]: boolean;
+  [key: `lesson${number}`]: boolean;
 }
 
-export interface users {
-    _id: ObjectId;
-    username: string;
-    full_name: string;
-    email: string;
-    password: string;
-    bookmarks: string[];
-    progress: Lesson[];
-}
+export type users = {
+  _id: ObjectId;
+  username: string;
+  full_name: string;
+  email: string;
+  password: string;
+  bookmarks: string[];
+  progress: Lesson[];
+};
 
-export interface flashcards {
-    _id: ObjectId;
-    unit: number;
-    section: string;
-    title: string;
-    body: string[];
-    img_url: string;
-}
+export type flashcards = {
+  _id: ObjectId;
+  unit: number;
+  section: string;
+  title: string;
+  body: string[];
+  img_url: string;
+};
 
-export interface flashcardsResponse {
-    flashcards: flashcards[];
-}
+export type flashcardsResponse = {
+  flashcards: flashcards[];
+};
 
-export interface articles {
-    _id: ObjectId;
-    title: string;
-    link: string;
-    img_url: string;
-    body: string;
-    source: string;
-    votes: number;
-}
+export type articles = {
+  _id: ObjectId;
+  title: string;
+  link: string;
+  img_url: string;
+  body: string;
+  source: string;
+  votes: number;
+};
 
-export interface updateFields {
-  username?: string,
-  password?: string,
-  full_name?: string,
-  email?: string,
-  bookmarks?: string
-}
+export type updateFields = {
+  username?: string;
+  password?: string;
+  full_name?: string;
+  email?: string;
+  bookmarks?: string;
+};
 
-export interface Saves {
-    type: string;
-    _id: string;
-}
+export type Saves = {
+  type: string;
+  _id: string;
+};
